498e49cf90c79d6b8dfc2b8477f9c7bbf47c7719
Ibwedagama <wardana.wedagama@gmail.com>
2022-01-08 18:37:17 +0700

    refactor: refactor permission plugin

--- src/plugin/permission.js
+++ src/plugin/permission.js
@@ -3,11 +3,15 @@ export default {
     Vue.prototype.$permission = function(value) {
       const permissions = this.$store.state.permissions;
 
-     if (value) {
-       if (Array.isArray(permissions) && permissions.includes(value)) {
-         return true
-       }
+      // return false immediately when value is falsy
+     if (!value) {
+      return false
      }
+
+    //  check if value is included on permissions Array
+     if (Array.isArray(permissions) && permissions.includes(value)) {
+      return true
+    }
 
       return false;
     };
